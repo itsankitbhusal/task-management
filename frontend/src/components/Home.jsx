4866a0b48d4166dac732ad902c4524040cc44843
itsankitbhusal <ankitbhusal95@gmail.com>
2023-11-30 21:24:47 +0545

    navbar and outlet

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,12 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "./Navbar"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
   )
 }
 
