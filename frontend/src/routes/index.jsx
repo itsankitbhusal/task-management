6f370cfa7c5ed51c2e5a113ce2c5d151582b5c3e
itsankitbhusal <ankitbhusal95@gmail.com>
2023-11-30 21:27:41 +0545

    routes defined

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,14 +1,41 @@
-import Home from "../components/Home"
-import NotFound from "../components/NotFound"
+import Login from "../components/Login";
+import NotFound from "../components/NotFound";
+import AddTask from "../components/AddTask";
+import PrivateRoute from "../components/PrivateRoute";
+import Home from "../components/Home";
+import Register from "../components/Register";
 
-const routes =  [
-    {
-        path: "/",
-        element: <Home />
-    }, {
+const routes = [
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        index: true,
+        element: (
+          <PrivateRoute>
+            <AddTask />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
         path: "*",
-        element: <NotFound />
-    }
-]
+        element: <NotFound />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+];
 
 export default routes;
