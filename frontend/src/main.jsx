d865f36031cfcf78290075bc7a786f4146b8508b
itsankitbhusal <ankitbhusal95@gmail.com>
2023-11-30 21:23:40 +0545

    axios request interceptor used

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
 // toast
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+axios.interceptors.request.use(
+  (request) => {
+    if (request.url.includes("/task")) {
+      request.headers.Authorization = `Bearer ${localStorage.getItem(
+        "access"
+      )}`;
+      request.headers.refresh = localStorage.getItem("refresh");
+    }
+    return request;
+  },
+  (err) => Promise.reject(err)
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
     <App />
     <ToastContainer />
-  </React.StrictMode>,
-)
+  </>
+);
